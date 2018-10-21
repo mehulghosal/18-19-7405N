3743f9f37cd9a3fcf0ab47f2e1e9ef88551f9f41
mehulghosal <mehul.ghosal@gmail.com>
2018-10-20 22:10:46 -0400

    yo im like kinda drunk so idk if this makes sense

--- prosCode/src/opcontrol.cpp
+++ prosCode/src/opcontrol.cpp
@@ -13,10 +13,16 @@
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
+
+
+//declaring global vars, controler and motors
+pros::Controller master(pros::E_CONTROLLER_MASTER);z
+pros::Motor backLeftMtr(1);
+pros::Motor backRightMtr(2);
+pros::Motor frontLeftMtr(2);
+pros::Motor frontRightMrt(2);
+
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
 	while (true) {
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
 		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
@@ -24,18 +30,13 @@ void opcontrol() {
 		int left = master.get_analog(ANALOG_LEFT_Y);
 		int right = master.get_analog(ANALOG_RIGHT_Y);
 
-		left_mtr = left;
-		right_mtr = right;
-		pros::delay(20);
+		backRightMtr, frontRightMrt = right;
+		backLeftMtr, frontLeftMtr = left;
+		
 	}
 }
-/*
-void pid (int d){
-	motor1.move((d-motor.getpositon())*kp);
-	motor2.move((d-motor.getpositon())*kp);
-	motor3.move((d-motor.getpositon())*kp);
-	motor4.move((d-motor.getpositon())*kp);
+
+void drive(){
+
+
 }
-*/
-
-
