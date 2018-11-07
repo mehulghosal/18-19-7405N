a1be30137c7f23d89dc824f41436f8ae5d5cea79
jaffer-ali <alijaffer3000@gmail.com>
2018-11-07 15:21:54 -0500

    double pressing + button handler

--- newCode/src/opcontrol.cpp
+++ newCode/src/opcontrol.cpp
@@ -59,32 +59,94 @@ DIGITAL_B		9
 DIGITAL_Y		10
 DIGITAL_A		11 */
 bool buttons [12] = {false, false, false, false, false, false, false, false, false, false, false, false};
-
+bool keyPresses[12] = {false, false, false, false, false, false, false, false, false, false, false, false};
+//controller_digital_e_t buttonTypes[12] = {DIGITAL_L1, DIGITAL_L2, DIGITAL_R1, DIGITAL_R2, DIGITAL_UP, DIGITAL_DOWN, DIGITAL_LEFT, DIGITAL_RIGHT, DIGITAL_X, DIGITAL_B, DIGITAL_Y, DIGITAL_A}
 void buttonHandler(){
-	if (master.get_digital(DIGITAL_L1) == 1){buttons[0] = true;}
-	else{buttons[0] = false;}
-	if (master.get_digital(DIGITAL_L2) == 1){buttons[1] = true;}
-	else{buttons[1] = false;}
-	if (master.get_digital(DIGITAL_R1) == 1){buttons[2] = true;}
-	else{buttons[2] = false;}
-	if (master.get_digital(DIGITAL_R2) == 1){buttons[3] = true;}
-	else{buttons[3] = false;}
-	if (master.get_digital(DIGITAL_UP) == 1){buttons[4] = true;}
-	else{buttons[4] = false;}
-	if (master.get_digital(DIGITAL_DOWN) == 1){buttons[5] = true;}
-	else{buttons[5] = false;}
-	if (master.get_digital(DIGITAL_LEFT) == 1){buttons[6] = true;}
-	else{buttons[6] = false;}
-	if (master.get_digital(DIGITAL_RIGHT) == 1){buttons[7] = true;}
-	else{buttons[7] = false;}
-	if (master.get_digital(DIGITAL_X) == 1){buttons[8] = true;}
-	else{buttons[8] = false;}
-	if (master.get_digital(DIGITAL_B) == 1){buttons[9] = true;}
-	else{buttons[9] = false;}
-	if (master.get_digital(DIGITAL_Y) == 1){buttons[10] = true;}
-	else{buttons[10] = false;}
-	if (master.get_digital(DIGITAL_A) == 1){buttons[11] = true;}
-	else{buttons[11] = false;}
+	if((master.get_digital(DIGITAL_L1)) == 1 && keyPresses[0] == false){
+		buttons[0] = !buttons[0];
+		keyPresses[0] = true;
+	}
+	else if(master.get_digital(DIGITAL_L1) == 0){
+		keyPresses[0] = false;
+	}
+
+	if((master.get_digital(DIGITAL_L2)) == 1 && keyPresses[1] == false){
+		buttons[1] = !buttons[1];
+		keyPresses[1] = true;
+	}
+	else if(master.get_digital(DIGITAL_L2) == 0){
+		keyPresses[1] = false;
+	}
+	if((master.get_digital(DIGITAL_R1)) == 1 && keyPresses[2] == false){
+		buttons[2] = !buttons[2];
+		keyPresses[2] = true;
+	}
+	else if(master.get_digital(DIGITAL_R1) == 0){
+		keyPresses[2] = false;
+	}
+	if((master.get_digital(DIGITAL_R2)) == 1 && keyPresses[3] == false){
+		buttons[3] = !buttons[3];
+		keyPresses[3] = true;
+	}
+	else if(master.get_digital(DIGITAL_R2) == 0){
+		keyPresses[3] = false;
+	}
+	if((master.get_digital(DIGITAL_UP)) == 1 && keyPresses[4] == false){
+		buttons[4] = !buttons[4];
+		keyPresses[4] = true;
+	}
+	else if(master.get_digital(DIGITAL_UP) == 0){
+		keyPresses[4] = false;
+	}
+	if((master.get_digital(DIGITAL_DOWN)) == 1 && keyPresses[5] == false){
+		buttons[5] = !buttons[5];
+		keyPresses[5] = true;
+	}
+	else if(master.get_digital(DIGITAL_DOWN) == 0){
+		keyPresses[5] = false;
+	}
+	if((master.get_digital(DIGITAL_LEFT)) == 1 && keyPresses[6] == false){
+		buttons[6] = !buttons[6];
+		keyPresses[6] = true;
+	}
+	else if(master.get_digital(DIGITAL_LEFT) == 0){
+		keyPresses[6] = false;
+	}
+	if((master.get_digital(DIGITAL_RIGHT)) == 1 && keyPresses[7] == false){
+		buttons[7] = !buttons[7];
+		keyPresses[7] = true;
+	}
+	else if(master.get_digital(DIGITAL_RIGHT) == 0){
+		keyPresses[7] = false;
+	}
+	if((master.get_digital(DIGITAL_X)) == 1 && keyPresses[8] == false){
+		buttons[8] = !buttons[8];
+		keyPresses[8] = true;
+	}
+	else if(master.get_digital(DIGITAL_X) == 0){
+		keyPresses[8] = false;
+	}
+	if((master.get_digital(DIGITAL_B)) == 1 && keyPresses[9] == false){
+		buttons[9] = !buttons[9];
+		keyPresses[9] = true;
+	}
+	else if(master.get_digital(DIGITAL_B) == 0){
+		keyPresses[9] = false;
+	}
+	if((master.get_digital(DIGITAL_Y)) == 1 && keyPresses[10] == false){
+		buttons[10] = !buttons[10];
+		keyPresses[10] = true;
+	}
+	else if(master.get_digital(DIGITAL_Y) == 0){
+		keyPresses[10] = false;
+	}
+	if((master.get_digital(DIGITAL_A)) == 1 && keyPresses[11] == false){
+		buttons[11] = !buttons[1];
+		keyPresses[11] = true;
+	}
+	else if(master.get_digital(DIGITAL_A) == 0){
+		keyPresses[11] = false;
+	}
 }
 
 // void buttonReset(){
@@ -179,7 +241,7 @@ void drive(int driveL, int driveR) {
 	pros::lcd::print(7, "FR : %d", (frontRightMtr.get_actual_velocity()));
 
 }
- 
+
 void motorStop() {
 	for (int i = 0; i < 8; i++) {
 		motors[i] = 0;
