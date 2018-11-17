27b3f0e542f1eca3acf7614015304cc863a7da29
mehulghosal <mehul.ghosal@gmail.com>
2018-11-17 09:21:04 -0500

    fucking

--- newCode/src/autonomous.cpp
+++ newCode/src/autonomous.cpp
@@ -11,7 +11,7 @@ void leftTurn(int mult);
 int getAutonState();
 //AUTONS//
 void auton1(){
-	pros::lcd::print(0, "INIT auton1");
+	/*pros::lcd::print(0, "INIT auton1");
 	//move forward to ball in front
 	flywheel(true); // flywheel first, flameo - josh
 	moveTo(3000);//this is pretty close - 2900 is perfect to intake the ball
@@ -41,6 +41,38 @@ void auton1(){
 	pros::c::delay(2000);
 	reaper(1);
 	pros::c::delay(2000);
+	reaper(0);*/
+	//Bad code^^
+	pros::lcd::print(0, "INIT auton1");
+	//move forward to ball in front
+	flywheel(true); // flywheel first, flameo - josh
+	moveTo(3000);//this is pretty close - 2900 is perfect to intake the ball
+	//pros::c::delay(1000);
+	//turn on intake
+	intake(1);
+
+	pros::c::delay(3000);
+	
+
+	pros::c::delay(500);
+
+	//turn right 135 degrees
+	moveTo(-3200);
+	pros::c::delay(1000);
+	rightTurn(4.55);
+	pros::c::delay(500);
+
+	//reaper and flywheel shoot
+	//delay for flywheel to accelerate
+	pros::c::delay(1000);
+	reaper(1);
+	pros::c::delay(1500);
+	reaper(0);
+	moveTo(3000);
+	pros::c::delay(1800);
+	reaper(1);
+	pros::c::delay(2000);
+	pros::c::delay(3000);
 	reaper(0);
 }
 void auton2(){
@@ -53,27 +85,27 @@ void auton2(){
 	intake(1);
 
 	pros::c::delay(3000);
-	intake(0);
+	
 
 	pros::c::delay(500);
 
 	//turn right 135 degrees
-	moveTo(-3000);
+	moveTo(-3200);
 	pros::c::delay(1000);
-	leftTurn(7);
+	leftTurn(4.55);
 	pros::c::delay(500);
-	moveTo(-100);
 
 	//reaper and flywheel shoot
 	//delay for flywheel to accelerate
 	pros::c::delay(1000);
 	reaper(1);
-	pros::c::delay(2200);
+	pros::c::delay(1500);
 	reaper(0);
-	moveTo(2000);
-	pros::c::delay(2000);
+	moveTo(3000);
+	pros::c::delay(1800);
 	reaper(1);
 	pros::c::delay(2000);
+	pros::c::delay(3000);
 	reaper(0);
 }
 void auton3(){
@@ -86,32 +118,32 @@ void auton3(){
 	intake(1);
 
 	pros::c::delay(3000);
-	intake(0);
+	
 
 	pros::c::delay(500);
 
 	//turn right 135 degrees
-	moveTo(-3000);
+	moveTo(-3200);
 	pros::c::delay(1000);
-	rightTurn(7);
+	rightTurn(9.1);
 	pros::c::delay(500);
-	moveTo(2600);
 
 	//reaper and flywheel shoot
 	//delay for flywheel to accelerate
 	pros::c::delay(1000);
 	reaper(1);
-	pros::c::delay(2200);
+	pros::c::delay(1500);
 	reaper(0);
-	moveTo(2000);
-	pros::c::delay(2000);
+	moveTo(3000);
+	pros::c::delay(1800);
 	reaper(1);
 	pros::c::delay(2000);
+	pros::c::delay(3000);
 	reaper(0);
 
 }
 void auton4(){
-	pros::lcd::print(0, "INIT auton4");
+	pros::lcd::print(0,"INIT Auton4");
 	//move forward to ball in front
 	flywheel(true); // flywheel first, flameo - josh
 	moveTo(3000);//this is pretty close - 2900 is perfect to intake the ball
@@ -120,27 +152,27 @@ void auton4(){
 	intake(1);
 
 	pros::c::delay(3000);
-	intake(0);
+	
 
 	pros::c::delay(500);
 
 	//turn right 135 degrees
-	moveTo(-3000);
+	moveTo(-3200);
 	pros::c::delay(1000);
-	leftTurn(7);
+	leftTurn(9.1);
 	pros::c::delay(500);
-	moveTo(2600);
 
 	//reaper and flywheel shoot
 	//delay for flywheel to accelerate
 	pros::c::delay(1000);
 	reaper(1);
-	pros::c::delay(2200);
+	pros::c::delay(1500);
 	reaper(0);
-	moveTo(2000);
-	pros::c::delay(2000);
+	moveTo(3000);
+	pros::c::delay(1800);
 	reaper(1);
 	pros::c::delay(2000);
+	pros::c::delay(3000);
 	reaper(0);
 }
 void testAuton(){
--- newCode/src/opcontrol.cpp
+++ newCode/src/opcontrol.cpp
@@ -132,19 +132,19 @@ void testfunct(){
 	//turn right 135 degrees
 	moveTo(-3200);
 	pros::c::delay(1000);
-	rightTurn(6);
+	rightTurn(4.55);
 	pros::c::delay(500);
-	moveTo(-100);	
 
 	//reaper and flywheel shoot
 	//delay for flywheel to accelerate
 	pros::c::delay(1000);
 	reaper(1);
-	pros::c::delay(2200);
+	pros::c::delay(1500);
 	reaper(0);
-	moveTo(2000);
-	pros::c::delay(2000);
+	moveTo(3000);
+	pros::c::delay(1800);
 	reaper(1);
+	pros::c::delay(2000);
 	pros::c::delay(3000);
 	reaper(0);
 }
@@ -152,121 +152,121 @@ void testfunct(){
 void opcontrol() {
 
 	//fuck this all jeez
-	testfunct();
+	//testfunct();
 
-	// bool flyWheelToggle = false;
-	// int intakeToggle = 0;
-	// int reaperToggle = 0;
-	// bool xPressed = false;
-	// bool yPressed = false;
-	// bool left = false;
-	// bool right = false;
-	// bool bPressed = false;
-	// bool lPressed = false;
-	// bool rPressed = false;
+	bool flyWheelToggle = false;
+	int intakeToggle = 0;
+	int reaperToggle = 0;
+	bool xPressed = false;
+	bool yPressed = false;
+	bool left = false;
+	bool right = false;
+	bool bPressed = false;
+	bool lPressed = false;
+	bool rPressed = false;
 
-	// while (true) {
+	while (true) {
 
-	// 	int driveLeft = master.get_analog(ANALOG_LEFT_Y);
-	// 	int driveRight = master.get_analog(ANALOG_RIGHT_X);
+		int driveLeft = master.get_analog(ANALOG_LEFT_Y);
+		int driveRight = master.get_analog(ANALOG_RIGHT_X);
 
-	// 	if (master.get_digital(DIGITAL_X) == 1 && xPressed == false){
-	// 		if (reaperToggle == 1){
-	// 			reaperToggle = 0;
-	// 		}
-	// 		else if (reaperToggle == 0 || reaperToggle == -1){
-	// 			reaperToggle = 1;
-	// 		}
-	// 		xPressed = true;
-
-
-	// 	}
-	// 	else if(master.get_digital(DIGITAL_X) == 0) {
-	// 		xPressed = false;
-	// 	}
-
-	// 	if (master.get_digital(DIGITAL_B) == 1 && bPressed == false) {
-	// 		if (reaperToggle == -1){
-	// 			reaperToggle = 0;
-	// 		}
-	// 		else if (reaperToggle == 0 || reaperToggle == 1){
-	// 			reaperToggle = -1;
-	// 		}
-	// 		bPressed = true;
+		if (master.get_digital(DIGITAL_X) == 1 && xPressed == false){
+			if (reaperToggle == 1){
+				reaperToggle = 0;
+			}
+			else if (reaperToggle == 0 || reaperToggle == -1){
+				reaperToggle = 1;
+			}
+			xPressed = true;
 
 
-	// 	}
-	// 	else if (master.get_digital(DIGITAL_B) == 0) {
-	// 		bPressed = false;
-	// 	}
+		}
+		else if(master.get_digital(DIGITAL_X) == 0) {
+			xPressed = false;
+		}
 
-	// 	//intake toggling
-	// 	if (master.get_digital(DIGITAL_LEFT) == 1 && lPressed == false ){
+		if (master.get_digital(DIGITAL_B) == 1 && bPressed == false) {
+			if (reaperToggle == -1){
+				reaperToggle = 0;
+			}
+			else if (reaperToggle == 0 || reaperToggle == 1){
+				reaperToggle = -1;
+			}
+			bPressed = true;
 
-	// 		if (intakeToggle == 1){
-	// 			intakeToggle = 0;
-	// 		}
-	// 		else if (intakeToggle == 0 || intakeToggle == -1){
-	// 			intakeToggle = 1;
-	// 		}
-	// 		lPressed = true;
 
-	// 	}
-	// 	else if (master.get_digital(DIGITAL_LEFT) == 0){
-	// 		lPressed = false;
-	// 	}
+		}
+		else if (master.get_digital(DIGITAL_B) == 0) {
+			bPressed = false;
+		}
 
-	// 	if (master.get_digital(DIGITAL_RIGHT) == 1 && rPressed == false) {
+		//intake toggling
+		if (master.get_digital(DIGITAL_LEFT) == 1 && lPressed == false ){
 
-	// 		if (intakeToggle == -1){
-	// 			intakeToggle = 0;
-	// 		}
-	// 		else if (intakeToggle == 0 || intakeToggle == 1){
-	// 			intakeToggle = -1;
-	// 		}
-	// 		rPressed = true;
+			if (intakeToggle == 1){
+				intakeToggle = 0;
+			}
+			else if (intakeToggle == 0 || intakeToggle == -1){
+				intakeToggle = 1;
+			}
+			lPressed = true;
 
-	// 	}
-	// 	else if (master.get_digital(DIGITAL_RIGHT) == 0){
-	// 		rPressed = false;
-	// 	}
+		}
+		else if (master.get_digital(DIGITAL_LEFT) == 0){
+			lPressed = false;
+		}
 
-	// 	//flywheel toggling
-	// 	if (master.get_digital(DIGITAL_Y) == 1 && yPressed == false){
-	// 		flyWheelToggle = !flyWheelToggle;
-	// 		yPressed = true;
-	// 		pros::Task::delay(20);
-	// 	}
-	// 	else if (master.get_digital(DIGITAL_Y) == 0){
-	// 		yPressed = false;
-	// 	}
+		if (master.get_digital(DIGITAL_RIGHT) == 1 && rPressed == false) {
 
-	// 	// pros::lcd::print(2, "ARM: %d",(int)armMotor.get_position()) ;
-	// 	if(master.get_digital(DIGITAL_R2) == 1){
-	// 			armMotor = 100;
-	// 	}
-	// 	else if(master.get_digital(DIGITAL_L2) == 1){
-	// 			armMotor = -100;
-	// 	}
-	// 	else if(armMotor.get_position() >= 400){
-	// 		armMotor = -15;
-	// 	}
+			if (intakeToggle == -1){
+				intakeToggle = 0;
+			}
+			else if (intakeToggle == 0 || intakeToggle == 1){
+				intakeToggle = -1;
+			}
+			rPressed = true;
 
-	// 	else if(armMotor.get_position() < 400){
-	// 		armMotor = 15;
-	// 	}
+		}
+		else if (master.get_digital(DIGITAL_RIGHT) == 0){
+			rPressed = false;
+		}
 
-	// 	if (master.get_digital(DIGITAL_UP) == 1) {
-	// 		motorStop();
-	// 	}
-	// 	pros::lcd::print(1, "Reaper: %f Intake: %d", (reaperMotor.get_actual_velocity()),(int)intakeToggle);
-	// 	pros::lcd::print(2, "Flywheel: %f", (flyWheelMotor.get_actual_velocity()));
+		//flywheel toggling
+		if (master.get_digital(DIGITAL_Y) == 1 && yPressed == false){
+			flyWheelToggle = !flyWheelToggle;
+			yPressed = true;
+			pros::Task::delay(20);
+		}
+		else if (master.get_digital(DIGITAL_Y) == 0){
+			yPressed = false;
+		}
 
-	// 	intake(intakeToggle);
-	// 	flywheel(flyWheelToggle);
-	// 	drive(driveLeft, driveRight);
-	// 	reaper(reaperToggle);
-	// 	pros::Task::delay(20);
+		// pros::lcd::print(2, "ARM: %d",(int)armMotor.get_position()) ;
+		if(master.get_digital(DIGITAL_R2) == 1){
+				armMotor = 100;
+		}
+		else if(master.get_digital(DIGITAL_L2) == 1){
+				armMotor = -100;
+		}
+		else if(armMotor.get_position() >= 400){
+			armMotor = -15;
+		}
 
-	// }
+		else if(armMotor.get_position() < 400){
+			armMotor = 15;
+		}
+
+		if (master.get_digital(DIGITAL_UP) == 1) {
+			motorStop();
+		}
+		pros::lcd::print(1, "Reaper: %f Intake: %d", (reaperMotor.get_actual_velocity()),(int)intakeToggle);
+		pros::lcd::print(2, "Flywheel: %f", (flyWheelMotor.get_actual_velocity()));
+
+		intake(intakeToggle);
+		flywheel(flyWheelToggle);
+		drive(driveLeft, driveRight);
+		reaper(reaperToggle);
+		pros::Task::delay(20);
+
+	}
 }
