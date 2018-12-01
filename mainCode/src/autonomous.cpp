70738fdc9cf1741acf44b8ce793403b47ca5b48b
KeshabGate <keshabgate@gmail.com>
2018-11-30 19:05:24 -0500

    replicated top blue to top red

--- mainCode/src/autonomous.cpp
+++ mainCode/src/autonomous.cpp
@@ -23,10 +23,6 @@ void auton1(){
 	//pros::c::delay(1000);
 
 	pros::c::delay(500);
-
-
-//	pros::c::delay(500);
-
 	moveTo(-2850);
 	rightTurn(101);
 	//moveReaper(2000, true);
@@ -55,6 +51,32 @@ void auton1(){
 void auton2(){
 	pros::lcd::print(0, "INIT auton2");
 	flywheel(true); // flywheel first, flameo - josh
+	intake(1);
+	pros::c::delay(1500);
+	moveTo(3000);//this is pretty close - 2900 is perfect to intake the ball
+	//pros::c::delay(1000);
+	pros::c::delay(500);
+	moveTo(-2850);
+	leftTurn(101);
+	//moveReaper(2000, true);
+	reaper(1);
+	pros::c::delay(1000);
+	reaper(0);
+	//pros::c::delay(1000);
+	moveTo(1700);
+	reaper(1);
+	pros::c::delay(3000);
+	reaper(0);
+	leftTurn(14);
+	moveTo(1200);
+
+
+	//park
+	moveTo(-1200);
+	rightTurn(14);
+	moveTo(-1700 - 600);
+	rightTurn(90);
+	moveTo(2500);
 
 }
 void auton3(){
@@ -67,13 +89,13 @@ void auton4(){
 	pros::lcd::print(0,"INIT auton4");
 	flywheel(true); // flywheel first, flameo - josh
 
-	moveTo(3000);
+	/*moveTo(3000);
 	intake(1);
 	pros::c::delay(2000);
 	turn(83, 0);
 
 	reaper(1);
-	pros::c::delay(700);
+	pros::c::delay(700);*/
 	// reaper(0);
 	// moveTo(1000);
 	// reaper(1);
